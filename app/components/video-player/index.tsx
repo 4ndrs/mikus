e699f3760c732e08ae1c5b8f21bc95843bbade4f
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-06-24 15:19:59 +0200

    Add fade out animation & change controls hiding logic

--- app/components/video-player/index.tsx
+++ app/components/video-player/index.tsx
@@ -18,6 +18,8 @@ const VideoPlayer = (props: Props) => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [loop, setLoop] = useState(props.loop);
   const [openMenu, setOpenMenu] = useState(false);
+  const [showControls, setShowControls] = useState(true);
+  const [hidingControls, setHidingControls] = useState(false);
 
   const videoRef = useRef<HTMLVideoElement>(null);
   const menuPositionRef = useRef<{ x: number; y: number }>({ x: 0, y: 0 });
@@ -67,7 +69,15 @@ const VideoPlayer = (props: Props) => {
   };
 
   return (
-    <div className={props.className + " group/player relative bg-black"}>
+    <div
+      onMouseEnter={() => setShowControls(true)}
+      onMouseLeave={() => {
+        if (isPlaying) {
+          setHidingControls(true);
+        }
+      }}
+      className={props.className + " group/player relative bg-black"}
+    >
       <video
         loop={props.loop}
         ref={videoRef}
@@ -87,18 +97,26 @@ const VideoPlayer = (props: Props) => {
         } fixed h-12 w-24 items-center justify-center gap-2 rounded bg-black`}
       >
         <div
-          onClick={() => {
-            setOpenMenu(false);
-          }}
+          onClick={() => setOpenMenu(false)}
           className="fixed inset-0 bg-transparent"
         />
         <HeartFilled className="text-red-600" /> Mikus
       </div>
 
       <div
-        className={` ${
-          isPlaying ? "hidden group-hover/player:flex" : "flex"
-        } absolute bottom-0 left-0 right-0 mx-6 animate-fade-in flex-col gap-4 bg-black/20 p-2 [&_svg]:text-[1.4rem] [&_svg]:text-white`}
+        onAnimationEnd={() => {
+          if (hidingControls) {
+            setHidingControls(false);
+            setShowControls(false);
+          }
+        }}
+        className={`${
+          !showControls
+            ? "hidden"
+            : hidingControls
+            ? "flex animate-fade-out"
+            : "flex animate-fade-in"
+        } absolute bottom-0 left-0 right-0 mx-6 flex-col gap-4 bg-black/20 p-2 [&_svg]:text-[1.4rem] [&_svg]:text-white`}
       >
         <ProgressBar videoRef={videoRef} isPlaying={isPlaying} />
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,11 +17,16 @@ module.exports = {
       },
       animation: {
         "fade-in": "fade-in 250ms ease-out",
+        "fade-out": "fade-out 250ms ease-in forwards",
       },
       keyframes: {
         "fade-in": {
           "0%": { opacity: "0" },
           "100%": { opacity: "1" },
+        },
+        "fade-out": {
+          "0%": { opacity: "1" },
+          "100%": { opacity: "0" },
         },
       },
     },
