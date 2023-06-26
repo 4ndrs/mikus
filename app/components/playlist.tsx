43742063e53e40004907c266b04be3412a9b8b3c
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-06-26 18:55:08 +0200

    Add some initial list styles

--- app/components/playlist.tsx
+++ app/components/playlist.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 type Props = { title: string };
 
 const Playlist = ({ title }: Props) => (
@@ -7,7 +9,23 @@ const Playlist = ({ title }: Props) => (
       <p className="my-1 text-xs text-gray-400">1 / 2</p>
     </div>
 
-    <div className="bg-gray-900">hello</div>
+    <div className="bg-gray-900">
+      <div className="flex pb-1 pr-2 pt-2">
+        <span className="w-6 self-center text-center text-xs">1</span>
+        <div className="relative">
+          <Image
+            src="/mikus.thumbnail.png"
+            width="128"
+            height="128"
+            alt="mikus thumbnail"
+          />
+          <span className="absolute bottom-0 right-0 m-1 rounded bg-black px-1 py-[0.19rem] text-xs font-medium">
+            00:45
+          </span>
+        </div>
+        <span className="px-2 text-sm font-medium">mikus.webm</span>
+      </div>
+    </div>
   </div>
 );
 
