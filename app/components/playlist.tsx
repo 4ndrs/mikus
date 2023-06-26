591bfa77ebe0dbb65bc53182c226b5a2e6c8f6d2
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-06-26 18:28:46 +0200

    Adjust playlist header margins

--- app/components/playlist.tsx
+++ app/components/playlist.tsx
@@ -2,7 +2,7 @@ type Props = { title: string };
 
 const Playlist = ({ title }: Props) => (
   <div className="max-h-[28.75rem] overflow-hidden rounded-xl border border-gray-700">
-    <div className="bg-gray-800 pl-4 pr-[0.38rem] pt-3">
+    <div className="bg-gray-800 pb-1 pl-4 pr-[0.38rem] pt-3">
       <h1 className="text-xl font-bold">{title}</h1>
       <p className="my-1 text-xs text-gray-400">1 / 2</p>
     </div>
