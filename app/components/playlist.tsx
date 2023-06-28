5b30298ccee6905a94a5ff1238159f3f14fc6f17
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-06-28 23:45:29 +0200

    Use aside tag for the playlist & unordered list for the links

--- app/components/playlist.tsx
+++ app/components/playlist.tsx
@@ -10,7 +10,7 @@ type Props = {
 };
 
 const Playlist = ({ title, videos, selectedId }: Props) => (
-  <div className="overflow-hidden rounded-xl border border-gray-700">
+  <aside className="overflow-hidden rounded-xl border border-gray-700">
     <div className="bg-gray-800 pb-1 pl-4 pr-[0.38rem] pt-3">
       <h1 className="text-xl font-bold">{title}</h1>
       <p className="my-1 text-xs text-gray-400">
@@ -19,33 +19,31 @@ const Playlist = ({ title, videos, selectedId }: Props) => (
       </p>
     </div>
 
-    <div className="max-h-[23rem] overflow-y-scroll bg-gray-900 [&::-webkit-scrollbar]:hidden">
+    <ul className="max-h-[23rem] overflow-y-scroll bg-gray-900 [&::-webkit-scrollbar]:hidden">
       {videos.map((video, index) => (
-        <Link
-          key={video.id}
-          href={`/?v=${video.id}`}
-          className="flex pb-1 pr-2 pt-2"
-        >
-          <span className="w-6 self-center text-center text-xs">
-            {index + 1}
-          </span>
-          <div className="relative">
-            <Image
-              src={video.thumbnail}
-              width="128"
-              height="128"
-              alt={`${video.title}'s thumbnail`}
-              className="h-14 w-[6.25rem] object-cover"
-            />
-            <span className="absolute bottom-0 right-0 m-1 rounded bg-black/75 px-1 py-[0.19rem] text-xs font-medium">
-              {video.duration}
+        <li key={video.id}>
+          <Link href={`/?v=${video.id}`} className="flex pb-1 pr-2 pt-2">
+            <span className="w-6 self-center text-center text-xs">
+              {index + 1}
             </span>
-          </div>
-          <h2 className="flex-1 px-2 text-sm font-medium">{video.title}</h2>
-        </Link>
+            <div className="relative">
+              <Image
+                src={video.thumbnail}
+                width="128"
+                height="128"
+                alt={`${video.title}'s thumbnail`}
+                className="h-14 w-[6.25rem] object-cover"
+              />
+              <span className="absolute bottom-0 right-0 m-1 rounded bg-black/75 px-1 py-[0.19rem] text-xs font-medium">
+                {video.duration}
+              </span>
+            </div>
+            <h2 className="flex-1 px-2 text-sm font-medium">{video.title}</h2>
+          </Link>
+        </li>
       ))}
-    </div>
-  </div>
+    </ul>
+  </aside>
 );
 
 export default Playlist;
