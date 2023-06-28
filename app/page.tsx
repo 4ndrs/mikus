1dc7e515540fa03cc5fe57c9b93a00248bdddf11
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-06-28 14:57:45 +0200

    Add some descriptions

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ const videos = [
   {
     id: "4Tg7e2P9R1",
     title: "mikus.webm",
+    description: "dancing miku",
     src: "/mikus.webm",
     duration: "00:45",
     thumbnail: "/mikus.thumbnail.png",
@@ -16,6 +17,7 @@ const videos = [
   {
     id: "6Fh5d8J2K7",
     title: "1472407089017.webm",
+    description: "dancing miku V2",
     src: "/1472407089017.webm",
     duration: "00:26",
     thumbnail: "/1472407089017.thumbnail.png",
@@ -23,6 +25,7 @@ const videos = [
   {
     id: "9Rj4k6E7H1",
     title: "racing_miku.webm",
+    description: "vrooom vrooom miku",
     src: "/racing_miku.webm",
     duration: "00:36",
     thumbnail: "/racing_miku.thumbnail.png",
@@ -30,6 +33,7 @@ const videos = [
   {
     id: "2Sg9h1K8D3",
     title: "MIKU.webm",
+    description: "miku!!!",
     src: "/MIKU.webm",
     duration: "00:21",
     thumbnail: "/MIKU.thumbnail.png",
@@ -37,6 +41,7 @@ const videos = [
   {
     id: "3Mj6k7L9N4",
     title: "Magical Candy ミカヅキBIGWAVE.webm",
+    description: "cute miku",
     src: "/Magical Candy ミカヅキBIGWAVE.webm",
     duration: "00:22",
     thumbnail: "/Magical Candy ミカヅキBIGWAVE.thumbnail.png",
@@ -44,6 +49,7 @@ const videos = [
   {
     id: "8Bn5v3C4X9",
     title: "Astrophysics-Ms-Miku-After-Dark-cover.webm",
+    description: "melancholic miku",
     src: "/Astrophysics-Ms-Miku-After-Dark-cover.webm",
     duration: "00:27",
     thumbnail: "/Astrophysics-Ms-Miku-After-Dark-cover.thumbnail.png",
@@ -51,6 +57,7 @@ const videos = [
   {
     id: "1Ft9g7Y3Q6",
     title: "Arelice - Kissing Me hard.webm",
+    description: "cool miku",
     src: "/Arelice - Kissing Me hard.webm",
     duration: "02:56",
     thumbnail: "/Arelice - Kissing Me hard.thumbnail.png",
--- app/types.ts
+++ app/types.ts
@@ -1,6 +1,7 @@
 export interface Video {
   id: string;
   title: string;
+  description: string;
   src: string;
   duration: string;
   thumbnail: string;
