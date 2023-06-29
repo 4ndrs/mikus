20d442a0746e692c431997371a3ad4ee94d6151a
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-06-29 20:37:17 +0200

    Improve progress custom hook update consistency

--- app/hooks/use-progress.ts
+++ app/hooks/use-progress.ts
@@ -1,44 +1,55 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 const useProgress = (videoRef: React.RefObject<HTMLVideoElement>) => {
   const [progress, setProgress] = useState(0);
   const [buffered, setBuffered] = useState(0);
-
-  const lastUpdateRef = useRef(0);
 
   useEffect(() => {
     if (!videoRef.current) {
       return;
     }
 
+    let id: NodeJS.Timer;
+
+    const milliseconds = 350;
     const videoElement = videoRef.current;
 
     const handleLoadedData = () => {
       setBuffered(videoElement.buffered.end(0));
+      clearInterval(id);
     };
 
-    const handleTimeUpdate = () => {
-      const current = videoElement.currentTime;
-      const difference = (current - lastUpdateRef.current) * 1000;
+    const update = () => {
+      setProgress(videoElement.currentTime);
 
-      // Update after x milliseconds
-      const milliseconds = 300;
-
-      if (difference < 0 || difference > milliseconds) {
-        setProgress(current);
-        lastUpdateRef.current = current;
-
-        if (videoElement.buffered.length > 0) {
-          setBuffered(videoElement.buffered.end(0));
-        }
+      if (videoElement.buffered.length > 0) {
+        setBuffered(videoElement.buffered.end(0));
       }
     };
 
-    videoElement.addEventListener("timeupdate", handleTimeUpdate);
+    const handlePlay = () => {
+      id = setInterval(update, milliseconds);
+    };
+
+    const handlePause = () => {
+      clearInterval(id);
+    };
+
+    // the options are decrease ms or handler below, TODO: find alternative
+    const handleSeeking = () => setProgress(videoElement.currentTime);
+
     videoElement.addEventListener("loadeddata", handleLoadedData);
+    videoElement.addEventListener("play", handlePlay);
+    videoElement.addEventListener("pause", handlePause);
+    videoElement.addEventListener("seeking", handleSeeking);
 
     return () => {
-      videoElement.removeEventListener("timeupdate", handleTimeUpdate);
+      videoElement.removeEventListener("loadeddata", handleLoadedData);
+      videoElement.removeEventListener("play", handlePlay);
+      videoElement.removeEventListener("pause", handlePause);
+      videoElement.removeEventListener("seeking", handleSeeking);
+
+      clearInterval(id);
     };
   }, [videoRef]);
 
